9170e9b89dbaf44a78bfe5bce2ecbf7b31b1d867
Tim Jones <65367108+timfmjones@users.noreply.github.com>
2022-07-25 17:33:14 -0400

    Add files via upload

---
+++ app.R
@@ -0,0 +1,93 @@
+library(shiny)
+
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("SearcHit"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      numericInput("val", 
+                   "Valence", 
+                   value = 0.5),
+      
+      numericInput("temp", 
+                   "Tempo", 
+                   value = 150),
+      
+      numericInput("danc", 
+                   "Danceability", 
+                   value = 0.5),
+      
+      numericInput("energy", 
+                   "Energy", 
+                   value = 0.5),
+    
+      numericInput("inst", 
+                   "Instrumentalness", 
+                   value = 0.5),
+      
+      numericInput("dur", 
+                   "Duration (in ms)", 
+                   value = 250000),
+      
+      numericInput("key", 
+                   "Key", 
+                   value = 6),
+      
+      selectInput("select", "Genre", 
+                  choices = list("Rock" = "Rock", "Pop" = "Pop",
+                                 "Hip Hop" = "Hip Hop", "Rap" = "Rap", "R&B" = "R&B", "Metal" = "Metal", "Blues" = "Blues", "Country" = "Country", "Dance/Electronic" = "Dance/Electronic", "Easy Listening" = "Easy Listening", "Folk/Acoustic" = "Folk/Acoustic", "Latin" = "Latin",  "World/Traditional" = "World/Traditional", "Set()" = "Set()"), selected = 1),
+      width = 25),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      verbatimTextOutput("prediction"),
+      
+      renderText({input$val}),
+      renderText({input$temp}),
+      renderText({input$danc}),
+      renderText({input$dur}),
+      renderText({input$key}),
+      renderText({input$select}),
+      # Output: Histogram ----
+      #plotOutput(outputId = "distPlot")
+      
+    )
+  )
+)
+
+
+server <- function(input, output) {
+  
+  output$prediction <- renderPrint({
+    
+    ss <- readr::read_csv('spotify_songs.csv')
+    ss <- na.omit(ss)
+    full_model <- lm(popularity~duration_ms+tempo+danceability+valence+energy+instrumentalness, data=ss)
+    
+    newsong <- data.frame(duration_ms=c({input$dur}),tempo=c({input$temp}),danceability=c({input$danc}),valence=c({input$val}),energy=c({input$energy}),instrumentalness=c({input$inst}))
+    predict(full_model, newsong)
+    predict(full_model, newsong, interval = "prediction", level = 0.05)
+  })
+  
+ 
+  
+  # Histogram of the Old Faithful Geyser Data ----
+  # with requested number of bins
+  # This expression that generates a histogram is wrapped in a call
+  # to renderPlot to indicate that:
+  #
+  # 1. It is "reactive" and therefore should be automatically
+  #    re-executed when inputs (input$bins) change
+  # 2. Its output type is a plot
+ 
+  
+}
+
+shinyApp(ui = ui, server = server)
